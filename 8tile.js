9a0b010566dd416c5bb14ad3b290c1342aa90df2
Spencer Moran <smoran02@gmail.com>
2013-05-15 19:41:48 -0700

    Add 8tile.html, 8tile.js and 8tilestyle.css

---
+++ 8tile.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    
+    function changeColor() {
+        $(this).toggleClass('lighttile');
+    };
+
+    function makeNewPuzzle() {
+        for (var i = 0; i < numbers.length; i++) {
+            var index = Math.floor(Math.random() * 8);
+            var temp = numbers[i];
+            numbers[i] = numbers[index];
+            numbers[index] = temp;
+        }
+
+        for (var i = 0; i < 9; i++) {
+            var j = i + 1;
+            var x = "sq" + j;
+            var y = document.getElementById(x);
+            if (numbers[i] == 0) {
+                y.className = y.className + " blank";
+                y.innerHTML = "";
+            }
+            else {
+                y.innerHTML = numbers[i];
+            }
+        }
+    };
+
+    var numbers = [0,1,2,3,4,5,6,7,8];
+
+    makeNewPuzzle();    
+    
+    $('.tile').on('mouseenter', changeColor).on('mouseleave', changeColor);
+
+    $('.newGame').on('click', function() {
+        $('.blank').addClass('tile');
+        $('.blank').removeClass('blank');
+        makeNewPuzzle();
+    });
+
+    $('.tile').on('click', function() {
+        $('.blank').addClass('tile');
+        $('.blank').text($(this).text());
+        $('.blank').removeClass('blank');
+        $(this).addClass('blank');
+        $(this).removeClass('tile');
+        $(this).text("");
+    });
+});
